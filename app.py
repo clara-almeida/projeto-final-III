cb47a64084b03f2e6dbcdd69abab93678267693b
clara-almeida <clara.almeidagom@gmail.com>
2024-04-10 13:20:41 -0300

    teste3

--- app.py
+++ app.py
@@ -1,31 +1,17 @@
 import os
 import requests
 from flask import Flask, render_template, request, redirect, url_for
-from raspador import get_discursos
+from raspador import get_epbr
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-
 app = Flask(__name__) # Cria uma instância do Flask. 
-    
 
-@app.route("/")
-def apresentacao():
-  return render_template('apresentacao.html')
+@app.route("/epbr")
+def epbr():
+    run_epbr = get_epbr()
+    return render_template('epbr.html')
 
-@app.route("/contato")
-def contato():
-  return render_template('contato.html') 
-
-@app.route("/materias")
-def materias():
-  return render_template('materias.html') 
-
-@app.route("/raspador-discursos")
-def discursos():
-    discursos = get_discursos()
-    html = render_template("discursos.html", discursos=discursos)
-    return html
 
 if __name__ == "__main__":
     app.run(debug=True)
--- raspador.py
+++ raspador.py
@@ -8,7 +8,7 @@ from openai import OpenAI
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-arquivo_credenciais = "trabalho-final-aa-574920153065.json"
+arquivo_credenciais = "clipping-bot-418820-52ef5e8ed495.json"
 conteudo_credenciais = os.environ["KEY_GOOGLE"]
 with open(arquivo_credenciais, mode="w") as arquivo:
     arquivo.write(conteudo_credenciais)
@@ -18,36 +18,25 @@ planilha = api.open_by_key(os.environ["SHEET_ID"])
 sheet = planilha.worksheet("Página1")
 
 
-def get_discursos():
 
-    chave_api = os.environ.get("OPENAI_API_KEY")
-    client = OpenAI(api_key=chave_api)
+chave_api = os.environ.get("OPENAI_API_KEY")
+client = OpenAI(api_key=chave_api)
 
-    #captura página "Últimos discursos e pronunciamentos"
-    result = requests.get("https://www.gov.br/planalto/pt-br/acompanhe-o-planalto/discursos-e-pronunciamentos")
+def get_epbr():
+    #captura página "Ultimas Noticias EPBR"
+    result = requests.get("https://epbr.com.br/ultimas/")
 
     #variável result vai receber o codigo da execução da página chamada pela request
     soup = BeautifulSoup(result.text)
 
-    titles = soup.find_all("h2", class_="tileHeadline")
+    titles = soup.find_all("h3", class_="jeg_post_title")
 
     # Itera sobre as tags encontradas para extrair os títulos e os links
-    discursos = []
-
-    for i, title in enumerate(titles):
-        if i>=3:
-           break
+    for title in titles:
         # Extrai o texto do título
         title_text = title.a.text.strip()
         # Extrai o link
         link = title.a["href"]
-
-        link_cells = sheet.findall(link, in_column=2)
-        if link_cells:
-            link_cell=link_cells[0]
-            row_values = sheet.row_values(link_cell.row)
-            discursos.append({"title": row_values[0], "link": row_values[1], "content":row_values[2]})
-            continue
 
         # Faz uma solicitação HTTP para obter o conteúdo da página do link
         link_response = requests.get(link)
@@ -57,23 +46,34 @@ def get_discursos():
         link_soup = BeautifulSoup(link_html, "html.parser")
 
         # Extrai o conteúdo dos parágrafos principais
-        main_paragraphs = link_soup.find_all("p", attrs={'class': None})
+        main_paragraphs = link_soup.find("div", class_="content-inner").find_all("p")
 
 
         # Concatena o texto dos parágrafos principais
         main_text = "\n".join(paragraph.get_text().strip() for paragraph in main_paragraphs)
 
-        #Integração com a OpenAI
 
-        prompt = "Você é um jornalista e está escrevendo uma matéria sobre este discurso do presidente da república. Analise o conteúdo e selecione, sem realizar nenhuma alteração no texto, o trecho de maior relevância para uma matéria jornalística com um limite de 280 caracteres. Este trecho precisa ter relação com a temática principal do discurso. Responda somente com o trecho, não acrescente nenhuma palavra ou justificativa."
-        response = client.chat.completions.create(
-            model="gpt-4-1106-preview",
-            messages=[{"role": "user", "content": f"{prompt}: {main_text}"}
-        ])
-        discursos.append({"title": title_text, "link": link, "content":response.choices[0].message.content})            
+        coluna_links = sheet.col_values(2) #testa para ver se o link já está na coluna e já foi lido, para evitar repetições.
 
-        
-        linha_armazenar = [title_text, link,response.choices[0].message.content] #armazenar título, link e resumo na planilha
-        sheet.insert_row(linha_armazenar, 2)
-        
-    return discursos
+        if link in coluna_links:
+            print("Link já raspado")
+
+        else:
+            if "Enauta" in main_text:
+                prompt = "Resuma a matéria abaixo, referente a uma empresa de petróleo de capital aberto, em um texto de até 300 caracteres. Esse resumo será utilizado pela alta gerência para analisar rapidamente o conteúdo da matéria."
+
+                response = client.chat.completions.create(
+                    model="gpt-4-1106-preview",
+                    messages=[{"role": "user", "content": f"{prompt}: {main_text}"}
+                ])    
+
+                resumo_text = response.choices[0].message.content
+
+                linha_armazenar = [title_text, link,response.choices[0].message.content] #armazenar título, link e resumo na planilha
+                sheet.insert_row(linha_armazenar, 2)
+                token = os.environ.get("TOKEN_TELEGRAM")
+                chat_id = "-1002115892927"
+
+                url = f"https://api.telegram.org/bot{token}/sendMessage"
+                params = {"chat_id": chat_id, "text": 'Título: '+ title_text + '\n\n' + 'Link: ' + link + '\n\n' + 'Resumo:' + resumo_text}
+                response = requests.post(url, params=params)
